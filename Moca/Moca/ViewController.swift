a5a3975499ed21fb1fe820da29ab7eb3b009d644
kyeongmipak <qkrrudal7@naver.com>
2021-02-24 11:56:32 +0900

    2/24_moca PJT 초기 생성_kyeongmi

---
+++ Moca/Moca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moca
+//
+//  Created by 박경미 on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

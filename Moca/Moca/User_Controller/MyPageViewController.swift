1f4a327dd3e869956253afb39922d0bfd1452b8b
asd8169 <asd8169@icloud.com>
2021-03-03 12:38:52 +0900

    2021.03.03 inwoo : profile ImageUpload 및 profile image 디자인 변경

--- Moca/Moca/User_Controller/MyPageViewController.swift
+++ Moca/Moca/User_Controller/MyPageViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
-
-class MyPageViewController: UIViewController {
+import GoogleSignIn // Google Login import
+import KakaoSDKUser // Kakao Login import
+import NaverThirdPartyLogin // Naver Login import
+import UserNotifications // pushNotifications import
+class MyPageViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var alertImg: UIImageView!
     
@@ -15,6 +18,15 @@ class MyPageViewController: UIViewController {
     @IBOutlet weak var myNickName: UILabel!
     @IBOutlet weak var myImg: UIImageView!
     // *******************
+    
+    // 프로필: url, imagePickerController 변수
+    let imagePickerController = UIImagePickerController()
+    var imageURL: URL?
+    // ImageSelectModelProtocol itemdownload 변수
+    var userEmail = ""
+    
+    private var observer: NSObjectProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +35,23 @@ class MyPageViewController: UIViewController {
         alertImg.addGestureRecognizer(tapAlert)
         alertImg.isUserInteractionEnabled = true
         // ++++++++++++++++++++++++++++++++++++++++
+        
+        
+        // 이미지 둥글게 만들기
+        myImg.layer.cornerRadius = (myImg.frame.size.width) / 2
+        myImg.layer.masksToBounds = true
+        
+        // imagePickerController
+        imagePickerController.delegate = self
+        
+        // 이미지뷰 클릭 제스처 설정
+        myImg.isUserInteractionEnabled = true
+        let event = UITapGestureRecognizer(target: self,
+                                           action: #selector(clickMethod))
+        myImg.addGestureRecognizer(event)
+        
+        print("userEmail",Share.userEmail)
+    
     }
     
     // 알림 설정 기능 구현해서 넣기 -> 사진 바뀌는 것만 추가함
@@ -45,6 +74,27 @@ class MyPageViewController: UIViewController {
     
     // 로그아웃 기능 구현
     @IBAction func btnLogout(_ sender: UIButton) {
+        let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
+        
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("logout() success.")
+                
+            }
+        }
+        
+        GIDSignIn.sharedInstance()?.signOut()
+        
+        loginInstance?.requestDeleteToken()
+        // Share 초기화
+        Share.userEmail = ""
+        Share.userName = ""
+        
+        
+        navigationController?.popToRootViewController(animated: true)
     }
     
     // 회원탈퇴 기능 구현
@@ -59,5 +109,51 @@ class MyPageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    // profile image click 메소드
+    @objc func clickMethod() {
+        print("tapped")
+        let photoAlert = UIAlertController(title: "사진 가져오기", message: "Photo Library에서 사진을 가져 옵니다.", preferredStyle: UIAlertController.Style.actionSheet) // Alert가 화면 밑에서 돌출
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { [self]ACTION in
+            self.imagePickerController.sourceType = .photoLibrary
+            self.present(self.imagePickerController, animated: false, completion: nil) // animated: true로 해서 차이점을 확인해 보세요!
+            print("imagePickerController \(String(describing: present))")
+            
+            
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        photoAlert.addAction(okAction)
+        photoAlert.addAction(cancelAction)
+        
+        present(photoAlert, animated: true, completion: nil)
+        
+    }
+    
+    
+    // Photo Library에서 사진 가져오기(함수 이름만 입력하면 준비된 함수임). Print해보면 위치를 알 수 있음.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { // 원본 이미지가 있을 경우
+            
+            myImg.image = image
+            print("check : \(image)")
+            imageURL = info[UIImagePickerController.InfoKey.imageURL] as? URL
+            print("imageURL : \(imageURL!)")
+            
+        }
+    
+        print("nil value check : \(String(describing: imageURL))")
+        
+        let imageUploadModel = ImageUploadModel()
+        if imageURL == nil { //예외 처리
+            
+        }else{
+            imageUploadModel.uploadImageFile(userEmail: userEmail, at: imageURL!, completionHandler: {_,_ in print("Upload Success")})
+        }
+        
+        
+        // 켜놓은 앨범 화면 없애기
+        dismiss(animated: true, completion: nil)
+    }
 }
---
+++ Moca/Moca/profile_Json/imageUpLoadModel.swift
@@ -0,0 +1,91 @@
+//
+//  imageUpLoadModel.swift
+//  Moca
+//
+//  Created by  p2noo on 2021/03/03.
+//
+
+import Foundation
+class ImageUploadModel:NSObject{
+                    
+        // MARK: Img Upload
+        func buildBody(with fileURL: URL, parameters: [String: String]?) -> Data? {
+            // 파일을 읽을 수 없다면 nil을 리턴
+            guard let filedata = try? Data(contentsOf: fileURL) else {
+                return nil
+            }
+            
+            // 바운더리 값을 정하고,
+            // 각 파트의 헤더가 될 라인들을 배열로 만든다.
+            // 이 배열을 \r\n 으로 조인하여 한 덩어리로 만들어서
+            // 데이터로 인코딩한다.
+            let boundary = "XXXXX"
+            let mimetype = "image/jpeg"
+            let headerLines = ["--\(boundary)",
+                "Content-Disposition: form-data; name=\"file\"; filename=\"\(fileURL.lastPathComponent)\"",
+                "Content-Type: \(mimetype)",
+                "\r\n"]
+            var data = headerLines.joined(separator:"\r\n").data(using:.utf8)!
+            
+            // 그 다음에 파일 데이터를 붙이고
+            data.append(contentsOf: filedata)
+            data.append(contentsOf: "\r\n".data(using: .utf8)!)
+            
+            // 일반적인 데이터 넣을때 사용하는 폼
+            // --\(boundary)\r\n
+            // Content-Disposition: form-data; name=\"값\"\r\n\r\n
+            // 내용\r\n
+            let lines = ["--\(boundary)","Content-Disposition: form-data; name=\"name\"\r\n","values\r\n"]
+            data.append(contentsOf: lines.joined(separator: "\r\n").data(using: .utf8)!)
+            /*
+            if parameters != nil {
+                for (key, value) in parameters! {
+                    let lines = ["--\(boundary)","Content-Disposition: form-data; name=\"\(key)\"\r\n","\(value)\r\n"]
+                    data.append(contentsOf: lines.joined(separator: "\r\n").data(using: .utf8)!)
+                }
+            }
+            */
+            
+            // 마지막으로 데이터의 끝임을 알리는 바운더리를 한 번 더 사용한다.
+            // 이는 '새로운 개행'이 필요하므로 앞에 \r\n이 있어야 함에 유의 한다.
+            data.append(contentsOf: "\r\n--\(boundary)--".data(using:.utf8)!)
+            return data
+        }
+        
+        func uploadImageFile(userEmail: String, at filepath: URL, completionHandler: @escaping(Data?, URLResponse?) -> Void) {
+            
+            // 경로를 준비하고
+            //let url = URL(string: "\(filepath), ImageUpload.jsp")!
+
+            //var url = URL(string: "http://localhost:8080/ios/ImageUpload.jsp")!
+            var url = "http://localhost:8080/moca/jsp/ImageUpload.jsp"
+            let urlAdd = "?userEmail=\(userEmail)"
+            url = url + urlAdd
+            url = url.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+            let urlURL = URL(string: url)
+            
+            let parameters = [
+                "name" : ""
+    //            "content" : content,
+    //            "User_uSeqno" : String(USERSEQNO),
+    //            "Recipe_rSeqno" : String(RECIPESEQNO)
+            ]
+
+            // 경로로부터 요청을 생성한다. 이 때 Content-Type 헤더 필드를 변경한다.
+            var request = URLRequest(url: urlURL!)
+            request.httpMethod = "POST"
+            request.setValue("multipart/form-data; boundary=\"XXXXX\"",
+                             forHTTPHeaderField: "Content-Type")
+            
+            // 파일URL로부터 multipart 데이터를 생성하고 업로드한다.
+            if let data = buildBody(with: filepath, parameters: parameters) {
+                let task = URLSession.shared.uploadTask(with: request, from: data){ data, res, _ in
+                    completionHandler(data, res)
+                }
+                task.resume()
+            }
+        }
+        
+    } // -----
+
+

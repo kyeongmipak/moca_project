ece3f24136219e2f8869877b1ced72cfe9b83d42
김대환 <e994301s@gmail.com>
2021-03-05 01:28:07 +0900

    2021.03.05 아이디, 비밀번호 찾기 완료 - 대환

--- Moca/Moca/User_Controller/EmailPwSearchViewController.swift
+++ Moca/Moca/User_Controller/EmailPwSearchViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class EmailPwSearchViewController: UIViewController {
+class EmailPwSearchViewController: UIViewController, SearchEmailPwProtocol {
+  
+    
 
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var phoneTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var emailLabel: UILabel!
     @IBOutlet var searchButton: UIButton!
+    
+    var segmentCheck = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,7 @@ class EmailPwSearchViewController: UIViewController {
     @IBAction func idPwSegment(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
+            segmentCheck = 0
             emailLabel.isHidden = true
             emailTextField.isHidden = true
             emailTextField.text = ""
@@ -32,6 +37,7 @@ class EmailPwSearchViewController: UIViewController {
             phoneTextField.text = ""
             searchButton.setTitle("아이디 찾기", for: .normal)
         case 1:
+            segmentCheck = 1
             emailLabel.isHidden = false
             emailTextField.isHidden = false
             emailTextField.text = ""
@@ -44,7 +50,75 @@ class EmailPwSearchViewController: UIViewController {
     }
     
     @IBAction func actionSearchButton(_ sender: UIButton) {
+        var nameNilCheck = checkNil(str: nameTextField.text!)
+        var phoneNilCheck = checkNil(str: phoneTextField.text!)
+        var emailNilCheck = checkNil(str: emailTextField.text!)
+        
+        let searchIdPwModel = SearchEmailPwModel()
+        searchIdPwModel.delegate = self
+        
+        switch segmentCheck {
+        case 0:
+            if nameNilCheck != 0 && phoneNilCheck != 0 {
+                searchIdPwModel.searchEmailItems(userInformationName: nameTextField.text!, userInformationPhone: phoneTextField.text!)
+            } else {
+                let resultAlert = UIAlertController(title: "실패", message: "모든 항목을 입력해주세요.", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
+                    self.navigationController?.popViewController(animated: true)
+                })
+                resultAlert.addAction(onAction)
+                self.present(resultAlert, animated: true, completion: nil)
+            }
+        case 1:
+            if nameNilCheck != 0 && phoneNilCheck != 0 && emailNilCheck != 0{
+                searchIdPwModel.searchPasswordItems(userInformationEmail: emailTextField.text!, userInformationName: nameTextField.text!, userInformationPhone: phoneTextField.text!)
+            } else {
+                let resultAlert = UIAlertController(title: "실패", message: "모든 항목을 입력해주세요.", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
+                    self.navigationController?.popViewController(animated: true)
+                })
+                resultAlert.addAction(onAction)
+                self.present(resultAlert, animated: true, completion: nil)
+            }
+        default:
+            break
+        }
     }
     
+    func itemDownloaded(items: String) {
+        var segmentName = ""
+        switch segmentCheck {
+        case 0:
+            segmentName = "아이디"
+        case 1:
+            segmentName = "비밀번호"
+        default:
+            break
+        }
+        if items == ""{
+            let resultAlert = UIAlertController(title: "\(segmentName) 찾기 실패", message: "일치하는 정보가 없습니다.\n입력하신 정보를 다시 한 번 확인해 주세요.", preferredStyle: UIAlertController.Style.alert)
+            let cancelAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+            resultAlert.addAction(cancelAction)
+            self.present(resultAlert, animated: true, completion: nil)
+        } else {
+            let resultAlert = UIAlertController(title: "\(segmentName) 찾기 성공", message: "\(nameTextField.text!) 님의 \(segmentName)는 \(items) 입니다.", preferredStyle: UIAlertController.Style.alert)
+            let onAction = UIAlertAction(title: "로그인 화면으로 이동하기", style: UIAlertAction.Style.default, handler: {ACTION in
+                self.navigationController?.popViewController(animated: true)
+            })
+            let cancelAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+            resultAlert.addAction(onAction)
+            resultAlert.addAction(cancelAction)
+            self.present(resultAlert, animated: true, completion: nil)
+        }
+    }
+    
+    func checkNil(str: String) -> Int {
+        let check = str.trimmingCharacters(in: .whitespacesAndNewlines)
+        if check.isEmpty{
+            return 0
+        } else {
+            return 1
+        }
+    }
 
 }
---
+++ Moca/Moca/User_Json/SearchEmailPwModel.swift
@@ -0,0 +1,79 @@
+//
+//  SearchEmailPwModel.swift
+//  Moca
+//
+//  Created by 김대환 on 2021/03/05.
+//
+
+import Foundation
+
+protocol SearchEmailPwProtocol: class {
+    func itemDownloaded(items: String)
+}
+
+class SearchEmailPwModel: NSObject {
+    var delegate : SearchEmailPwProtocol!
+    var result = ""
+    
+    
+    func searchEmailItems(userInformationName : String, userInformationPhone : String) {
+        var urlPath = "http://127.0.0.1:8080/moca/jsp/SearchEmail.jsp"
+        let urlAdd = "?userInformationName=\(userInformationName)&userInformationPhone=\(userInformationPhone)"
+        urlPath = urlPath+urlAdd
+        let url = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error)in
+            if error != nil{
+                print("Failed to download data")
+            }else{
+                print("Data is downloading")
+                self.parseJSON(data!)
+            }
+        }
+        task.resume()
+    }
+    func searchPasswordItems(userInformationEmail : String, userInformationName : String, userInformationPhone : String) {
+        var urlPath = "http://127.0.0.1:8080/moca/jsp/SearchPassword.jsp"
+        let urlAdd = "?userInformationEmail=\(userInformationEmail)&userInformationName=\(userInformationName)&userInformationPhone=\(userInformationPhone)"
+        urlPath = urlPath+urlAdd
+        let url = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error)in
+            if error != nil{
+                print("Failed to download data")
+            }else{
+                print("Data is downloading")
+                self.parseJSON(data!)
+            }
+        }
+        task.resume()
+    }
+    func parseJSON(_ data: Data) {
+        var jsonResult = NSArray()
+        
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSArray
+        }catch let error as NSError{
+            print(error)
+        }
+        
+        var jsonElement = NSDictionary()
+        
+        for i in 0..<jsonResult.count{
+            
+            jsonElement = jsonResult[i] as! NSDictionary
+            print(jsonElement)
+            
+            if let count = jsonElement["result"] as? String{
+                result = count
+                print("count : \(count)")
+            }
+        }
+        
+        DispatchQueue.main.async(execute: {() -> Void in
+            self.delegate.itemDownloaded(items: self.result)
+        })
+    }
+}

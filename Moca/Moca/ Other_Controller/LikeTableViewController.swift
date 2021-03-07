16d7d3f79dd93f38cd10c8644a865c9613eabf30
ParkJiEun <qkrwldms011@naver.com>
2021-03-07 12:15:26 +0900

    지은 21.03.07 : 찜 수정중

--- Moca/Moca/ Other_Controller/LikeTableViewController.swift
+++ Moca/Moca/ Other_Controller/LikeTableViewController.swift
@@ -14,47 +14,51 @@ class LikeTableViewController: UITableViewController, LikeJsonModelProtocol, Lik
     
     // 최종적으로 tableView에서 사용할 배열 함수
     var LikeItem: NSMutableArray = NSMutableArray() // 배열 (한번 정해지면 바꿀 수 없음.)
+    // NSMutableArray → arraylist (추가 가능)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // instance 선언
+        let likeJsonModel = LikeJsonModel()
+        likeJsonModel.delegate = self // jsonModel에서 일 시키고, 그걸 self(여기서 쓸거임)
+        likeJsonModel.downloadItems(userEmail: Share.userEmail) // jsonModel에서 이 메소드 실행해서 일 처리해!
         
-//        // instance 선언
-//        let likeJsonModel = LikeJsonModel()
-//        let likeJsonModel.delegate = self // jsonModel에서 일 시키고, 그걸 self(여기서 쓸거임)
-//        likeJsonModel.downloadItems() // jsonModel에서 이 메소드 실행해서 일 처리해!
-//
-//        // Custom Cell 정의
+        // Custom Cell 정의
         likeListTableView.rowHeight = 80 // Cell 높이 지정
     }
+    
     
     //4 > creat function for add your custom code
     func likeDeleteTapped(cell:LikeTableViewCell)
     {
         //Get the indexpath of cell where button was tapped
-//        let indexPath001 = self.tableView.indexPath(for: cell)//genrate your clicked cell indexPath
-//        self.LikeItem.remove(indexPath001 as Any)
-//        self.LikeItem.removeObject(at:(indexPath001?.row)!)
-//
-//
-//        let addressNo = Int(cell.likeBrand!)
-//        let likeDeleteModel = LikeDeleteModel() // instance 선언
-//        _ = likeDeleteModel.likeDeleteItems(addressNo: addressNo!)
-//        tableView.deleteRows(at:[indexPath001!], with: .left)
-//        tableView.reloadData()
-//        print(indexPath001!.row)
+        let indexPath001 = self.tableView.indexPath(for: cell)//genrate your clicked cell indexPath
+        self.LikeItem.remove(indexPath001 as Any)
+        self.LikeItem.removeObject(at:(indexPath001?.row)!)
+
+        
+
+        let menuNo = Int(cell.menuNO.text!)
+        let likeDeleteModel = LikeDeleteModel() // instance 선언
+        _ = likeDeleteModel.likeDeleteItems(menuNO: menuNo!)
+        tableView.deleteRows(at:[indexPath001!], with: .left)
+        tableView.reloadData()
+        print(indexPath001!.row)
     }
     
     func likeItemDownloaded(items: NSArray) {
         // JsonModel의 locations에 담겨져서 넘어옴.
         LikeItem = items as! NSMutableArray
         self.likeListTableView.reloadData()
+        
     }
     // 입력, 수정, 삭제 후 DB 재구성 → Table 재구성
     override func viewWillAppear(_ animated: Bool) {
         print("viewWillAppear")
         let likeJsonModel = LikeJsonModel() // protocol연결된 클래스 객체 선언
         likeJsonModel.delegate = self // 일 처리 시킬건데, 이 화면에서 시킬거고
-        likeJsonModel.downloadItems() // jsonModel의 downloadItems를 처리하게 할거야.
+        likeJsonModel.downloadItems(userEmail: Share.userEmail) // jsonModel의 downloadItems를 처리하게 할거야.
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -78,29 +82,19 @@ class LikeTableViewController: UITableViewController, LikeJsonModelProtocol, Lik
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // ↓ custom한 TableViewCell 사용할거니까
-        let cell = tableView.dequeueReusableCell(withIdentifier: "likeCell", for: indexPath) as! LikeTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LikeCell", for: indexPath) as! LikeTableViewCell
         
-//        cell.delegate=self as? LikeCellDelegate
+        cell.delegate = self as? MyCellDelegate
         // cell 정의
         // 현재 배열값으로 들어온 cell 풀어서 정의.
-//        let item: LikeDBModel = LikeItem[indexPath.row] as! LikeDBModel
-//        cell.likeBrand
-//        cell.likeName
-//        cell.likeImg
-//
-//        cell.lblName?.text = "\(item.address_addressNo!)"
+        let item: LikeDBModel = LikeItem[indexPath.row] as! LikeDBModel
+        cell.likeBrand?.text = "\(item.menuName!)"
+        cell.likeName?.text = "\(item.menuName!)"
+        cell.menuNO?.text = "\(item.menu_menuNo!)"
         
-        
+        cell.menuNO.isHidden = true
         return cell
     }
-    
-    //    func reloadTable() {
-    //        // instance 선언
-    //        let starJsonModel = StarJsonModel()
-    //        starJsonModel.delegate = self // jsonModel에서 일 시키고, 그걸 self(여기서 쓸거임)
-    //        starJsonModel.downloadItems() // jsonModel에서 이 메소드 실행해서 일 처리해!
-    //        self.starListTableView.reloadData()
-    //    }
     
     /*
      // Override to support conditional editing of the table view.
@@ -137,14 +131,28 @@ class LikeTableViewController: UITableViewController, LikeJsonModelProtocol, Lik
      }
      */
     
-    /*
+    
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
+        
+       if segue.identifier == "starDetail"{
+           // 사용자가 클릭한 위치는 sender가 알고있는데, 그 위치인 TableView Cell을 담을 변수 cell.
+           let cell = sender as! UITableViewCell
+           // 그 위치는 이제 indexPath에서 지정.
+           let indexPath = self.starListTableView.indexPath(for: cell)
+           // 보낼 컨트롤러 위치
+           let detailAdrViewController = segue.destination as! DetailAdrViewController
+        var addressNo = StarItem[(indexPath! as NSIndexPath).row] as! AddressModel
+        
+           // detailview의 receiveItem에 =~~~~를 보낸다.
+        detailAdrViewController.addressReceiveNo = addressNo.addressNo!
+       }
+        
      }
-     */
+     
+    
+   // *************************************************************************
     
 }
